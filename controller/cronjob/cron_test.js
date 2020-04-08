3be76d6512f90dc3bf50ef8aeb06513b0fc5271e
AParziale <angelo.parziale@democom.it>
2020-04-08 10:37:44 +0200

    riepilogo dati testuale regione Molise e migliorie

--- controller/cronjob/cron_test.js
+++ controller/cronjob/cron_test.js
@@ -6,7 +6,7 @@ const bot = new telegraf(process.env.TOKEN_BOT);
 const cron_test = async () => {
     try {
         console.info("------ [ ESEGUO CRONJOB DI TEST ] ------");
-        await bot.telegram.sendMessage(process.env.CHATID_TEST, "Il documento inviato rappresenta il bollettino regionale nazionale. I dati sono rilasciati direttamente dalla Protezione Civile. 🤞🏻🤞🏻");
+        await bot.telegram.sendMessage(process.env.CHATID_TEST, "Messagio per testare il cronjob. 🤞🏻🤞🏻");
     } catch (error) {
         console.error("[ERROR] => ", error);
         await bot.telegram.sendMessage(process.env.CHATID_TEST, 'Non è stato possibile recuperare informazioni per tale richiesta! Riprova con un altro comando');
