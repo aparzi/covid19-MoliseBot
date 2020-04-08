7d79cf5059033bface7885684e6ab2e300cbdd64
AParziale <angelo.parziale@democom.it>
2020-04-08 16:09:44 +0200

    fix bug

--- controller/cronjob/cron_test.js
+++ controller/cronjob/cron_test.js
@@ -54,4 +54,10 @@ const cron_test = async () => {
     }
 };
 
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
 module.exports = cron_test;
